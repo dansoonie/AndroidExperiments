78a8253921445e4fb7a12be137b623a257dc5953
Yoon-Soo Lee <dan.lee@jocoos.com>
2017-08-07 22:35:38 +0900

    etc: Add exoplayer test for hls

---
+++ app/src/main/java/com/dansoonie/experiments/category/video/customplayer/ExoHlsPlayer.java
@@ -0,0 +1,196 @@
+package com.dansoonie.experiments.category.video.customplayer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * Created by dansoonie on 17/8/7/.
+ */
+
+public class ExoHlsPlayer extends FrameLayout implements ExoPlayer.EventListener{
+  private static final String TAG = "ExoHlsPlayer";
+
+  private Context mContext;
+  private String mUserAgent;
+  private SimpleExoPlayerView mPlayerView;
+  private SimpleExoPlayer mPlayer;
+  private DataSource.Factory mMediaDataSourceFactory;
+  private MediaSource mMediaSource;
+  private TrackSelector mTrackSelector;
+  private boolean mShouldAutoPlay;
+  private int mResumeWindow;
+  private long mResumePosition;
+  private boolean mResumeOnResume = false;
+  private OnPlayerInitializedListener mOnPlayerInitializedListener;
+
+  public interface OnPlayerInitializedListener {
+    void onInitialized(ExoHlsPlayer player);
+  }
+
+  public void setOnPlayerInitializedListener(OnPlayerInitializedListener listener) {
+    mOnPlayerInitializedListener = listener;
+  }
+
+  public ExoHlsPlayer(@NonNull Context context) {
+    super(context);
+    onCreate(context);
+  }
+
+  public ExoHlsPlayer(@NonNull Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+    onCreate(context);
+  }
+
+  public ExoHlsPlayer(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    onCreate(context);
+  }
+
+  public void onCreate(Context context) {
+    mContext = context;
+    mShouldAutoPlay = true;
+    mUserAgent = Util.getUserAgent(mContext, "ExoPlayerDemo");
+    mMediaDataSourceFactory = buildDataSourceFactory(null);
+    mPlayerView = new SimpleExoPlayerView(context);
+    addView(mPlayerView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+  }
+
+  public DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+    return new DefaultDataSourceFactory(mContext, bandwidthMeter,
+        buildHttpDataSourceFactory(bandwidthMeter));
+  }
+
+  public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+    return new DefaultHttpDataSourceFactory(mUserAgent, bandwidthMeter);
+  }
+
+  private void initializePlayer() {
+    boolean needNewPlayer = mPlayer == null;
+    if (needNewPlayer) {
+      mTrackSelector = new DefaultTrackSelector();
+
+      mPlayer = ExoPlayerFactory.newSimpleInstance(mContext, new DefaultTrackSelector());
+      mPlayer.addListener(this);
+
+      mPlayerView.setPlayer(mPlayer);
+      mPlayer.setPlayWhenReady(mShouldAutoPlay);
+    }
+    if (mOnPlayerInitializedListener != null) {
+      mOnPlayerInitializedListener.onInitialized(this);
+    }
+  }
+
+  public void load(String path) {
+    Uri uri = Uri.parse(path);
+    mMediaSource = new HlsMediaSource(uri, mMediaDataSourceFactory, null, null);
+    mPlayer.prepare(mMediaSource);
+  }
+
+  public void onStart() {
+    Log.d(TAG, "onStart");
+    if (Util.SDK_INT > 23) {
+      initializePlayer();
+    }
+  }
+
+  public void onResume() {
+    Log.d(TAG, "onResume");
+    if ((Util.SDK_INT <= 23 || mPlayer == null)) {
+      initializePlayer();
+    }
+  }
+
+  public void onPause() {
+    Log.d(TAG, "onPause");
+    if (Util.SDK_INT <= 23) {
+      releasePlayer();
+    }
+  }
+
+  public void onStop() {
+    Log.d(TAG, "onStop");
+    if (Util.SDK_INT > 23) {
+      releasePlayer();
+    }
+  }
+
+  private void releasePlayer() {
+    if (mPlayer != null) {
+      mShouldAutoPlay = mPlayer.getPlayWhenReady();
+      if (mResumeOnResume) {
+        updateResumePosition();
+      }
+      mPlayer.release();
+      mPlayer = null;
+      mTrackSelector = null;
+    }
+  }
+
+  private void updateResumePosition() {
+    mResumeWindow = mPlayer.getCurrentWindowIndex();
+    mResumePosition = mPlayer.isCurrentWindowSeekable() ? Math.max(0, mPlayer.getCurrentPosition()) : C.TIME_UNSET;
+  }
+
+  @Override
+  public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+  }
+
+  @Override
+  public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+  }
+
+  @Override
+  public void onLoadingChanged(boolean isLoading) {
+
+  }
+
+  @Override
+  public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+  }
+
+  @Override
+  public void onPlayerError(ExoPlaybackException error) {
+
+  }
+
+  @Override
+  public void onPositionDiscontinuity() {
+
+  }
+
+  @Override
+  public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+  }
+}
--- app/src/main/java/com/dansoonie/experiments/category/video/exo/ExoPlayerHlsTestActivity.java
+++ app/src/main/java/com/dansoonie/experiments/category/video/exo/ExoPlayerHlsTestActivity.java
@@ -1,184 +1,56 @@
 package com.dansoonie.experiments.category.video.exo;
 
 import android.app.Activity;
-import android.graphics.SurfaceTexture;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.View;
-import android.widget.Button;
 
 import com.dansoonie.experiments.Log;
 import com.dansoonie.experiments.R;
-import com.dansoonie.experiments.videosamples.Videos;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+import com.dansoonie.experiments.category.video.customplayer.ExoHlsPlayer;
 
-public class ExoPlayerHlsTestActivity extends Activity implements TextureView.SurfaceTextureListener {
+public class ExoPlayerHlsTestActivity extends Activity {
   private static final String TAG = "Exo";
 
-  private TextureView textureView;
-  private Button buttonLoadFile;
-  private Button buttonLoadUrl;
-  private Button buttonPlayPause;
-  private Button buttonSeek;
-
-  private SimpleExoPlayer exoPlayer;
+  private ExoHlsPlayer mPlayer;
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.exo_behavior);
+    setContentView(R.layout.exo_hls_player);
 
     Log.ENABLE_DEBUG = true;
-
-    textureView = (TextureView) findViewById(R.id.texture_view);
-    textureView.setSurfaceTextureListener(this);
-    buttonLoadFile = (Button) findViewById(R.id.button_load_file);
-    buttonLoadUrl = (Button) findViewById(R.id.button_load_url);
-    buttonPlayPause = (Button) findViewById(R.id.button_play_pause);
-    buttonSeek = (Button) findViewById(R.id.button_seek);
-
-    buttonLoadFile.setEnabled(false);
-    buttonLoadUrl.setEnabled(false);
-    buttonPlayPause.setEnabled(false);
-    buttonSeek.setEnabled(false);
-
-    View.OnClickListener loadListener = new View.OnClickListener() {
+    mPlayer = (ExoHlsPlayer) findViewById(R.id.exo_view);
+    mPlayer.setResumeOnResume(true);
+    mPlayer.setOnPlayerInitializedListener(new ExoHlsPlayer.OnPlayerInitializedListener() {
       @Override
-      public void onClick(View v) {
-        if (v.equals(buttonLoadFile)) {
-          exoPlayer.prepare(buildMediaSource("https://s3.ap-northeast-2.amazonaws.com/gb-tv/95/broadcasts/184/vod.m3u8"));
-        } else {
-          exoPlayer.prepare(buildMediaSource(Videos.getURL(1)));
-        }
-        buttonLoadFile.setEnabled(false);
-        buttonLoadUrl.setEnabled(false);
-        buttonPlayPause.setEnabled(true);
-        buttonSeek.setEnabled(true);
-      }
-    };
-    buttonLoadFile.setOnClickListener(loadListener);
-    buttonLoadUrl.setOnClickListener(loadListener);
-
-    buttonPlayPause.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (exoPlayer.getPlayWhenReady()) {
-          Log.d(TAG, "Pausing");
-          exoPlayer.setPlayWhenReady(false);
-        } else {
-          Log.d(TAG, "Starting");
-          exoPlayer.setPlayWhenReady(true);
-        }
+      public void onInitialized(ExoHlsPlayer player) {
+        player.load("https://s3.ap-northeast-2.amazonaws.com/gb-tv/58/broadcasts/187/vod.m3u8");
       }
     });
 
-    buttonSeek.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        long duration = exoPlayer.getDuration();
-        long seekPosition = (long) (duration * Math.random());
-        exoPlayer.seekTo(seekPosition);
-      }
-    });
-
-    exoPlayer = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
-    exoPlayer.addListener(new ExoPlayer.EventListener() {
-      @Override
-      public void onTimelineChanged(Timeline timeline, Object manifest) {
-        Log.d(TAG, "TimelineChanged");
-      }
-
-      @Override
-      public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-        Log.d(TAG, "TracksChanged");
-      }
-
-      @Override
-      public void onLoadingChanged(boolean isLoading) {
-        Log.d(TAG, "LoadingChanged");
-      }
-
-      @Override
-      public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-        String state;
-        switch (playbackState) {
-          case ExoPlayer.STATE_IDLE:
-            state = "STATE_IDLE";
-            break;
-          case ExoPlayer.STATE_BUFFERING:
-            state = "STATE_BUFFERING";
-            break;
-          case ExoPlayer.STATE_READY:
-            state = "STATE_READY";
-            break;
-          case ExoPlayer.STATE_ENDED:
-            state = "STATE_ENDED";
-            break;
-          default:
-            state = "STATE_????";
-        }
-        Log.d(TAG, String.format("PlayerStateChanged - playWhenReady: %b, playbackState: %s", playWhenReady, state));
-      }
-
-      @Override
-      public void onPlayerError(ExoPlaybackException error) {
-        Log.d(TAG, "PlayerError");
-      }
-
-      @Override
-      public void onPositionDiscontinuity() {
-        Log.d(TAG, "PositionDiscontinuity");
-      }
-
-      @Override
-      public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
-        Log.d(TAG, "PlaybackRarametersChanged");
-      }
-    });
-  }
-
-  private MediaSource buildMediaSource(String path) {
-    Uri uri = Uri.parse(path);
-    return new ExtractorMediaSource(uri,
-        new DefaultDataSourceFactory(this, Util.getUserAgent(this, "test")),
-        new DefaultExtractorsFactory(), null, null);
   }
 
   @Override
-  public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-    buttonLoadFile.setEnabled(true);
-    buttonLoadUrl.setEnabled(true);
-    exoPlayer.setVideoSurface(new Surface(surface));
+  protected void onStart() {
+    super.onStart();
+    mPlayer.onStart();
   }
 
   @Override
-  public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
+  protected void onResume() {
+    super.onResume();
+    mPlayer.onResume();
   }
 
   @Override
-  public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-    return false;
+  protected void onPause() {
+    super.onPause();
+    mPlayer.onPause();
   }
 
   @Override
-  public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
+  protected void onStop() {
+    super.onStop();
+    mPlayer.onStop();
   }
 }
